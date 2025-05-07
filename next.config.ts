42e8f75fd108910f472cdedf0094a6b963db7f26
anandanair <anandanairaan@gmail.com>
2025-05-07 14:27:57 +0800

    build: add react-pdf dependency and configure webpack alias
    Added react-pdf as a dependency to support PDF rendering in the application. Additionally, configured webpack to alias canvas to false to avoid potential build issues related to the canvas module.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
+import { config } from "process";
 
 const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  },
   /* config options here */
 };
 
